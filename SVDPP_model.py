c7da6742eb118ae4d257e94310cebaa3ddc5f780
Duan  Longteng <loduan@ethz.ch>
2023-06-23 15:48:56 +0200

    changes on svdpp

--- SVDPP_model.py
+++ SVDPP_model.py
@@ -1,5 +1,5 @@
 import numpy as np
-from utils import _load_data_for_surprise, _convert_df_to_matrix
+from utils import _load_data_for_surprise, _convert_df_to_matrix, compute_rmse
 import os
 from surprise import SVDpp
 from surprise.model_selection import KFold
@@ -14,27 +14,30 @@ class SVDPP_model:
         self.algo = None
         self.out_path = './output/svdpp.csv'
 
-    def fit(self, df_train):
+    def train(self, df_train):
         data_train, _ = _convert_df_to_matrix(df_train, 10000, 1000)
         data = _load_data_for_surprise(data_train)
 
         trainset = data.build_full_trainset()
 
         # create SVDPP algorithm and train it
-        algorithm = SVDpp(n_factors=12, lr_all=0.085, n_epochs=50, reg_all=0.01, verbose=True)
+        algorithm = SVDpp(n_factors=9, lr_all=0.085, n_epochs=10, reg_all=0.01, verbose=True)
         algorithm.fit(trainset)
-
+        print('finish training')
         self.algo = algorithm
 
     def predict(self, df_test):
+        predictions = []
         with open(self.out_path, 'w+') as f:
             f.write('Id,Prediction\n')
-            test = df_test
-            for id in test['Id']:
-                row, col = id.split('_')
-                row = int(row[1:])
-                col = int(col[1:])
+            for i in range(df_test.shape[0]):
+                row, col = int(df_test['row'].values[i]), int(df_test['col'].values[i])
+                # row = int(row[1:])
+                # col = int(col[1:])
                 uid = row-1
                 iid = col-1
                 pred = self.algo.predict(uid, iid, verbose=False)
-                f.write('r{0}_c{1},{2}\n'.format(row, col, pred))
+                predictions.append(pred.est)
+                f.write('r{0}_c{1},{2}\n'.format(row, col, pred.est))
+        labels = df_test['Prediction'].values
+        print('RMSE: {:.4f}'.format(compute_rmse(predictions, labels)))
